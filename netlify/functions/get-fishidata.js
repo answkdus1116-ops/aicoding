dff54d9b007f1829762a59255bc954308f06d0b3
answkdus1116-ops <answkdus1116@goedu.kr>
2026-05-15 17:16:40 +0900

    Update get-fishidata.js

--- netlify/functions/get-fishidata.js
+++ netlify/functions/get-fishidata.js
@@ -1,20 +1,24 @@
-const axios = require('axios'); // axios 설치 필요 혹은 fetch 사용
-
+// get-fish-data.js (수정 버전)
 exports.handler = async (event) => {
-  // Netlify 설정에 저장한 실제 키를 여기서 꺼내 씁니다. (사용자에게 안 보임)
   const API_KEY = process.env.GEMINI_API_KEY; 
   const { imageContent } = JSON.parse(event.body);
 
   try {
-    const response = await axios.post(`https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`, {
-      requests: [{
-        image: { content: imageContent },
-        features: [{ type: 'OBJECT_LOCALIZATION' }]
-      }]
+    const response = await fetch(`https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        requests: [{
+          image: { content: imageContent },
+          features: [{ type: 'OBJECT_LOCALIZATION' }]
+        }]
+      })
     });
+
+    const data = await response.json();
     return {
       statusCode: 200,
-      body: JSON.stringify(response.data)
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
     };
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
